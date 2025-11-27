f33d449490abf651496fdb99dfe18044d80b8e1d
Bell Wu <wu.bell@outlook.com>
2025-11-27 14:54:15 -0500

    job to read and write 23169 sample

---
+++ slurm_jobs/23169_read_write.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -t 1:00
+#SBATCH --mem=32G
+#SBATCH -J 23169_read_write
+#SBATCH -p all
+#SBATCH -c 6
+#SBATCH -N 1
+#SBATCH --output=/cluster/home/bellwu/job_logs/23169_read_write%j.out
+#SBATCH --error=/cluster/home/bellwu/job_logs/23169_read_write%j.err
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate xen_reader
+
+python /Users/bellwu/Py_programming/Py_Xen_SCLC/analyses/02_xen_qc/scripts/xenium_zarr.py

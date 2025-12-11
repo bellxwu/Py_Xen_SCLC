eadb80f1181ab8a0a180bcd6dc16c1503ebdda9e
Bell Wu <wu.bell@outlook.com>
2025-12-11 11:54:24 -0500

    slurm script change

---
+++ analyses/02_xen_qc/slurm_jobs/SCLC_xen_pp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -t 1:00:00
+#SBATCH --mem=64G
+#SBATCH -J SCLC_xen_pp
+#SBATCH -p himem
+#SBATCH -c 4
+#SBATCH -N 1
+#SBATCH --output=/cluster/home/bellwu/Py_Xenium/job_logs/SCLC_xen_pp%j.out
+#SBATCH --error=/cluster/home/bellwu/Py_Xenium/job_logs/SCLC_xen_pp%j.err
+#SBATCH -D /cluster/home/bellwu/Py_Xenium
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate xen_reader
+
+python -u analyses/02_xen_qc/h4h_scripts/qc_pp_xen.py
--- analyses/02_xen_qc/slurm_jobs/xen_h5ad_write.sh
+++ analyses/02_xen_qc/slurm_jobs/xen_h5ad_write.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -t 1:00:00
 #SBATCH --mem=64G
-#SBATCH -J 23169_read_write
+#SBATCH -J xen_h5ad_write
 #SBATCH -p himem
 #SBATCH -c 4
 #SBATCH -N 1
-#SBATCH --output=/cluster/home/bellwu/Py_Xenium/job_logs/23169_read_write%j.out
-#SBATCH --error=/cluster/home/bellwu/Py_Xenium/job_logs/23169_read_write%j.err
+#SBATCH --output=/cluster/home/bellwu/Py_Xenium/job_logs/xen_h5ad_write%j.out
+#SBATCH --error=/cluster/home/bellwu/Py_Xenium/job_logs/xen_h5ad_write%j.err
 #SBATCH -D /cluster/home/bellwu/Py_Xenium
 
 source ~/miniconda3/etc/profile.d/conda.sh

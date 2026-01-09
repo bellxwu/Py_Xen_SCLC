3f5319a48aeda241783f1d276a15ca8cb1e114ea
Bell Wu <wu.bell@outlook.com>
2026-01-09 10:58:17 -0500

    Edit to include memory usage

--- analyses/02_xen_qc/h4h_scripts/single_pp_indiv_samples.py
+++ analyses/02_xen_qc/h4h_scripts/single_pp_indiv_samples.py
@@ -9,10 +9,13 @@ Purpose is to avoid batch effects seen originally with neighborhood analysis.
 @author: bellwu
 """
 # %% ---- 1.0 Setting up environment ----
+import os
 import scanpy as sc
 import anndata as ad
 import time
 from pyxenium import xen_config as xc
+import gc
+import psutil
 
 ## import dir from h4h cluster
 xen_dir = xc.bell_concat 
@@ -31,20 +34,47 @@ SCLC_sample_names = SCLC_xen.obs['sample'].unique()
 sample_masks = {i: SCLC_xen.obs['sample'] == i for i in SCLC_sample_names}
 ## select samples from mask
 SCLC_samples = {i: SCLC_xen[sample_masks.get(i)] for i in SCLC_sample_names}
-# %% ---- 3.0 Normalization and log1p transformation ----
+# %% ---- 3.0 Remove processed samples ----
+'''
+Code block to remove samples already pre-processed. To remove samples with same
+name. 
+1) Successful: SCLC_4462962
+'''
+del SCLC_samples['SCLC_4462962']
+# %% ---- 4.0 Sample preprocessing setup ----
 '''
 Preprocessing done iteratively through all SCLC samples
 '''
-# %% ---- 3.1 Sample preprocessing ----
+## setting up process object for memory tracking
+Proc = psutil.Process(os.getpid()) # processing object
+Proc.memory_info() # returns tuple of memory information
+ProcBytes = Proc.memory_info().rss # prints in bytes
+
+## Define function to convert bytes to human readable
+def BytesToHuman(n):
+    '''
+    Converts raw bytes into readable string
+    '''
+    symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+    prefix = {}
+    for i, s in enumerate(symbols):
+        prefix[s] = 2 ** ((i + 1) * 10)
+    for s in reversed(symbols):
+        if abs(n) >= prefix[s]:
+            value = float(n) / prefix[s]
+            return '%.2f%s' % (value, s) # change how many decimal points
+    return "%sB" % n
+# %% ---- 4.1 Loop to pre-process ----
 ## write loop across all samples
 for SampleName, SampleAnnData in SCLC_samples.items():
     '''
-    For looop for running the SCLC xenium samples. Input is given as a 
+    For loop for running the SCLC xenium samples. Input is given as a 
     dictionary with SampleName as the key and SampleAnnData as its values.
     '''
     InitialStart = time.perf_counter()
+    InitialMem = Proc.memory_info().rss
     # initial normalization
-    print(f"Starting analysis of {SampleName}")
+    print(f"\nStarting analysis of {SampleName}")
     if "counts" not in SampleAnnData.layers:
         SampleAnnData.layers["counts"] = SampleAnnData.X.copy()
     sc.pp.normalize_total(SampleAnnData, target_sum=100, inplace=True)
@@ -59,7 +89,7 @@ for SampleName, SampleAnnData in SCLC_samples.items():
     print(f"Number of PCs computed {SampleAnnData.obsm['X_pca'].shape}")
     # write pca .h5ad
     StartTime = time.perf_counter()
-    SampleAnnData.write(f"{SampleName}_pp.h5ad")
+    SampleAnnData.write(preprocessed_dir / f"{SampleName}_pp.h5ad")
     EndTime = time.perf_counter()
     print(f"Writing .h5ad of {SampleName} successful, total time {EndTime - StartTime}")
     
@@ -72,7 +102,7 @@ for SampleName, SampleAnnData in SCLC_samples.items():
     print(f"\nNeigbors of {SampleName} successful, total time {EndTime - StartTime}")
     # write neighbors .h5ad
     StartTime = time.perf_counter()
-    SampleAnnData.write(f"{SampleName}_pp.h5ad")
+    SampleAnnData.write(preprocessed_dir / f"{SampleName}_pp.h5ad")
     EndTime = time.perf_counter()
     print(f"Writing .h5ad of {SampleName} successful, total time {EndTime - StartTime}")
     
@@ -81,9 +111,9 @@ for SampleName, SampleAnnData in SCLC_samples.items():
     sc.tl.umap(SampleAnnData)
     EndTime = time.perf_counter()
     print(f"\nUMAP of {SampleName} successful, total time {EndTime - StartTime}")
-    # write neighbors .h5ad
+    # write UMAP .h5ad
     StartTime = time.perf_counter()
-    SampleAnnData.write(f"{SampleName}_pp.h5ad")
+    SampleAnnData.write(preprocessed_dir / f"{SampleName}_pp.h5ad")
     EndTime = time.perf_counter()
     print(f"Writing .h5ad of {SampleName} successful, total time {EndTime - StartTime}")
     
@@ -95,12 +125,22 @@ for SampleName, SampleAnnData in SCLC_samples.items():
                  directed=False)
     EndTime = time.perf_counter()
     print(f"\nLeiden of {SampleName} successful, total time {EndTime - StartTime}")
-    # write neighbors .h5ad
+    # write final .h5ad
     StartTime = time.perf_counter()
-    SampleAnnData.write(f"{SampleName}_pp.h5ad")
+    SampleAnnData.write(preprocessed_dir / f"{SampleName}_pp.h5ad")
     EndTime = time.perf_counter()
+    EndMem = Proc.memory_info().rss
     print(f"Writing .h5ad of {SampleName} successful, total time {EndTime - StartTime}")
     print(f"\n Total time taken to process sample {EndTime - InitialStart}")
+    print(f"Memory consumed for processing sample {BytesToHuman(EndMem - InitialMem)}")
+    
+    # delete SampleAnnData to free memory
+    PreDeleteMem = Proc.memory_info().rss
+    del SampleAnnData
+    gc.collect()
+    PostDeleteMem = Proc.memory_info().rss
+    print(f"\nMemory freed after deleting {SampleAnnData} = {BytesToHuman(PreDeleteMem - PostDeleteMem)}")
+
 
 
 
--- analyses/02_xen_qc/test_scripts/test_pp_bulk_pipeline.py
+++ analyses/02_xen_qc/test_scripts/test_pp_bulk_pipeline.py
@@ -13,14 +13,16 @@ import scanpy as sc
 import anndata as ad
 import time
 from pyxenium import xen_config as xc
-import pandas as pd
 import numpy as np
 from scipy.sparse import csr_matrix
+import psutil
+import os
+import gc
 
-## import Paths
-xen_dir = xc.xen_bwu
-SCLC_xen_path = xen_dir / "SCLC_xen.h5ad"
-SCLC_xen = ad.read_h5ad(SCLC_xen_path)
+# ## import Paths
+# xen_dir = xc.xen_bwu
+# SCLC_xen_path = xen_dir / "SCLC_xen.h5ad"
+# SCLC_xen = ad.read_h5ad(SCLC_xen_path)
 test_dir = xc.test_bwu
 # %% Creating a generator for random matrix for adata
 ## function to create random matrix
@@ -31,7 +33,7 @@ def gen_counts(times, dim=(100,1000)):
         yield csr_matrix(rng.poisson(1, dim), dtype=np.float32) 
         i += 1
 ## create two random matrix counts        
-c1, c2 = gen_counts(2)
+c1, c2 = gen_counts(2, dim=(1000,5000))
 ## 
 test_groups = {'SCLC_64312': c1, 'SCLC_4462962': c2 }
 test_groups = {k: ad.AnnData(v) for k, v in test_groups.items()}
@@ -40,42 +42,106 @@ test_groups = {k: ad.AnnData(v) for k, v in test_groups.items()}
 start = time.perf_counter()
 end = time.perf_counter()
 min(c1.shape)-1
+# %% testing memory with libraries
+proc = psutil.Process(os.getpid()) # processing object
+proc.memory_info() # returns tuple of memory information
+n = proc.memory_info().rss # prints in bytes'
+n
+## note there are 1024 bytes in 1 "kibibyte"
+def bytes2human(n):
+    '''
+    Converts raw bytes into readable string
+    '''
+    symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+    prefix = {}
+    for i, s in enumerate(symbols):
+        prefix[s] = 2 ** ((i + 1) * 10)
+    for s in reversed(symbols):
+        if abs(n) >= prefix[s]:
+            value = float(n) / prefix[s]
+            return '%.2f%s' % (value, s) # change how many decimal points
+    return "%sB" % n
+n
+bytes2human(n)
 # %% for loop
 for k, v in test_groups.items():
     # for PCA
+    StartMem = proc.memory_info().rss
     start = time.perf_counter()
-    sc.pp.pca(v, n_comps=(v.shape[0]-1))
+    sc.pp.pca(v, n_comps=(min(v.shape)-1))
     end = time.perf_counter()
-    print(f"\nTotal PCA run time for {k} is {end - start}")
+    print(f"\nMemory before PCA {StartMem}")
+    print(f"Total PCA run time for {k} is {end - start}")
     print(f"Number of pcas computed {v.obsm['X_pca'].shape}")
     start = time.perf_counter()
     v.write(test_dir / f"{k}_pp.h5ad")
     end = time.perf_counter()
+    EndMem = proc.memory_info().rss
     print(f"Total PCA write time for {k} is {end - start}")
+    print(f"Total memory after PCA {EndMem}")
+    print(f"Memory consumed {EndMem - StartMem}")
     
     # for neighbours
+    StartMem = proc.memory_info().rss
     start = time.perf_counter()
+    print(f"\nMemory before neighbors analysis {StartMem}")
     sc.pp.neighbors(v, 
-                    n_pcs=v.obsm['X_pca'].shape[1])
+                    n_pcs=min(v.obsm['X_pca'].shape))
     end = time.perf_counter()
+    EndMem = proc.memory_info().rss
     print(f"Neighbors runtime is {end - start}")
+    print(f"Total memory after neighbors analysis {EndMem}")
+    print(f"Memory consumed {EndMem - StartMem}")
     
     # leiden
+    StartMem = proc.memory_info().rss
     start = time.perf_counter()
+    print(f"\nMemory before leiden analysis {StartMem}")
     sc.tl.leiden(v,
-                 flavor="leidenalg",
+                 flavor="igraph",
                  n_iterations=2)
     end = time.perf_counter()
+    EndMem = proc.memory_info().rss
     print(f"Leiden runtime is {end - start}")
-    
+    print(f"Total memory after leiden analysis {EndMem}")
+    print(f"Memory consumed {EndMem - StartMem}")
+
+    # delete v to free memory
+    StartMem = proc.memory_info().rss
+    print(f"\nMemory before deleting v {StartMem}")
+    del v
+    gc.collect()
+    EndMem = proc.memory_info().rss
+    print(f"Total memory after deleting v {EndMem}")
+    print(f"Memory freed {StartMem - EndMem}")
 # %% check how many pcs
 SCLC_64312 = test_groups['SCLC_64312']
 SCLC_64312.uns
 print(f"\nTotal PCA run time for {SCLC_64312} is {end - start}")
 print("test")
-# %% check how many pcs for actual
+# %% final memory calculation
+b = 1121026048 - 781287424
+bytes2human(b)
+## the entire process consumed 324M of memory
 
-ZYX
+## compared to memory usage at start, memory use has increased 
+MemChange = proc.memory_info().rss - 781287424
+bytes2human(MemChange)
+
+## deleting variables to see if it improves memory
+MemBefore = proc.memory_info().rss
+del test_groups
+MemAfter = proc.memory_info().rss
+## see how much memory is saved
+bytes2human(MemBefore - MemAfter) # only 160Kb of memory saved 
+# %% Run garbage collect
+MemBefore = proc.memory_info().rss
+gc.collect()
+MemAfter = proc.memory_info().rss
+bytes2human(MemBefore - MemAfter)
+# %% removing values from dictionary
+test_dict = {'SCLC_64312': c1, 'SCLC_4462962': c2 }
+del test_dict['SCLC_64312']
 
 
 
@@ -86,3 +152,6 @@ ZYX
 
 
 
+
+
+# %%

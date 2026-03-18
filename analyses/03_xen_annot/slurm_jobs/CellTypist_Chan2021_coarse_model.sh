089b22ddaf9c5467ad81f6f07807906c08f3b069
Bell Wu <wu.bell@outlook.com>
2026-03-18 18:57:23 -0400

    sh correction

--- analyses/03_xen_annot/slurm_jobs/CellTypist_Chan2021_coarse_model.sh
+++ analyses/03_xen_annot/slurm_jobs/CellTypist_Chan2021_coarse_model.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -t 6:00:00
 #SBATCH --mem=100G
-#SBATCH -J CellTypist_Chan2021_model
+#SBATCH -J CellTypist_Chan2021_coarse_model
 #SBATCH -p veryhimem
 #SBATCH -c 10
 #SBATCH -N 1
-#SBATCH --output=/cluster/home/bellwu/Py_Xenium/job_logs/CellTypist_Chan2021_model%j.out
-#SBATCH --error=/cluster/home/bellwu/Py_Xenium/job_logs/CellTypist_Chan2021_model%j.err
+#SBATCH --output=/cluster/home/bellwu/Py_Xenium/job_logs/CellTypist_Chan2021_coarse_model%j.out
+#SBATCH --error=/cluster/home/bellwu/Py_Xenium/job_logs/CellTypist_Chan2021_coarse_model%j.err
 #SBATCH -D /cluster/home/bellwu/Py_Xenium
 
 source ~/miniconda3/etc/profile.d/conda.sh
